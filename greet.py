ed0281a2df1d8f26b7c806f2d31bf6b1e6de25ee
RiazAhmad9 <riazahmad9259@gmail.com>
2026-04-08 14:03:23 +0600

    Create greet.py

---
+++ greet.py
@@ -0,0 +1,7 @@
+# Script for practicing input and output
+Name = input("Enter your name:")
+Age = input("Enter your age:")
+From = input("Enter where are you from:")
+
+print(f"Nice to meet you,{Name}! You are {Age} years old from {From}.")
+input("\nPress Enter to exit...")
