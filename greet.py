689e3efface021a28ccebc7d1d9a26043d90cd47
RiazAhmad9 <riazahmad9259@gmail.com>
2026-04-08 14:16:18 +0600

    Update greet.py

--- greet.py
+++ greet.py
@@ -4,4 +4,5 @@ Age = input("Enter your age:")
 From = input("Enter where are you from:")
 
 print(f"Nice to meet you,{Name}! You are {Age} years old from {From}.")
+print(f"Nice to meet you, {Name}! You are {Age} years old from {From}.")
 input("\nPress Enter to exit...")
