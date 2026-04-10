54241ceddde8a7b500040da53bbbb75b3e3ac338
RiazAhmad9 <riazahmad9259@gmail.com>
2026-04-10 14:21:21 +0600

    cs50 lecture0 end half

---
+++ Cs50's introduction to python/Day2/hello.py
@@ -0,0 +1,15 @@
+# Print a greeting message
+
+# Prompt the user for their name
+name = input("What is your name? ")
+
+# Remove any leading or trailing whitespace and capitalize the first letter of each word
+name = name.strip().title()
+
+
+
+
+
+
+# Print a greeting message using an f-string
+print(f"Hello, {name}")
