5ce9817947a76989584d0744a06c2b0a50c2bacb
RiazAhmad9 <riazahmad9259@gmail.com>
2026-04-16 09:31:50 +0600

    modified the code and added a readme

--- Mini project/guess.py
+++ Mini project/guess.py
@@ -1,21 +1,26 @@
 # A number guessing game
 
-# Imports a random integer and stores into 'secret_num' variable
+# Imports a random number
 import random
-secret_num = random.randint(1, 100)
 
 # 'checker' function where user input gets checked and an answer is returned
-def checker(k):
+def checker(k, secret_num):
     if k == secret_num :
         return "correct"
     elif k < secret_num :
-        return "too low"
+        return "too low\nTry again."
     elif k > secret_num :
-        return "too high"
+        return "too high\nTry again."
 
 # 'main' function where user inserts a number and result gets print considering the conditions
 def main():
-    # Loop for the user to keep guessing number.
+    # A random integer between (1-100) is stored in 'secret_num' variable
+    secret_num = random.randint(1, 100)
+    # Variable 'att' initial value to 0 to count user attempts
+    att = 0
+    # Variable 'max_att' value to 10 for limiting user attempts
+    max_att = 10
+    # Loop for the user to keep guessing number
     while True:
         # If valid input is not found prints a specific message and continues the loop
         try:
@@ -23,13 +28,25 @@ def main():
         except ValueError:
             print("Enter a whole number.")
             continue
+        # Add 1 to 'att' variable
+        att += 1
+        # Breaks if 'att' is more than 'max_att'
+        if att >= max_att:
+            print(f"Out of guesses! The number is {secret_num}")
+            break
         # 'return' variable which stores the returned value from 'checker' function
-        result = checker(num)
+        result = checker(num, secret_num)
         # Prints conclusion
         print(result)
         # Breaks if conclusion is final else continues
         if result == "correct":
-            break
+            again = input("Play again? (y/n) ").lower()
+            # If 'again' variable value is set to y then runs 'main()' again else breaks.
+            if again == "y":
+                main()
+            elif again == "n":
+                print("Thanks for playing!")
+                break
 
 # Calls main
 if __name__ == "__main__":
---
+++ Mini project/readme.md
@@ -0,0 +1,22 @@
+# Number GUESSING GAME
+
+A command-line game where you try to guess between 1 and 100.
+
+## HOW TO RUN
+
+Run python guess.py
+
+## HOW TO PLAY
+
+- Enter a whole number between 1 and 100
+- The game will display too high or too low
+- You have 10 attempts to guess the correct number
+
+## Project Structure
+
+- checker() = verifies the answer and returns the result
+- main() = stores user input, track attempts, runs game loop and prints answer
+
+## Requirements
+
+- Python 3.x
